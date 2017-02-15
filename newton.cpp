06662dead417295ca4ea0a84f4b28c0f95c70697
Dragv <ale_guereca_valdivia@hotmail.com>
2017-02-15 11:01:32 -0600

    Update

--- newton.cpp
+++ newton.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <stdexcept>
 
 #define ITER 1000
 #define MINERR 1E-15
@@ -8,43 +9,49 @@ using namespace std;
 
 typedef double (* vFunctionCall)(double x);
 
+double abError(float x0, float x1){
+  return x1-x0;
+}
+
+double reError(float x0, float x1){
+  return (x1-x0)/x1;
+}
+
 double function1(double x) {
-    return x*x*x+5*x-10;
+    return exp(x) - pow(x, 2) + 3 * x - 2;
 }
 
 double functionDerivative (double x){
-    return 3*x*x+5;
+    return exp(x) - 2*x + 3;
 }
 
 float tangenteMethod(vFunctionCall funct, vFunctionCall deri, float a){
     float x0 = a;
-    double y = funct(x0);
-    int i = 1;
+    float x1;
+    float err;
+    int i = 0;
     do{
         if (deri(x0) == 0){
             throw invalid_argument("Math error");
         }
-
-        float x1 = x0 - (funct(x0)/deri(x0));
-        x0 = x1;
-
+        x1 = x0;
+        x0 = x1 - (funct(x1)/deri(x1));
+        err = abs(x0-x1);
         if (i > ITER){
-            return x0;
+            throw invalid_argument("Max number of iterations reached");
         }
-        x0 = x0 - (y/deri(x0));
-        y = funct(x0);
+        cout << "Iteracion " << i << ", Error absoluto iterativo " << abError(x0, a) << ", Error relativo iterativo " << reError(x0, a) << '\n';
         i++;
-        if (i > ITER){
-            return  x0;
-        }
-        i++;
-    } while (abs(funct(x0)) > MINERR);
+    } while (err >= MINERR);
     return x0;
 }
 
 int main() {
+    float x0;
+    cout << "x inicial" << '\n';
+    cin >> x0;
 
-    cout << tangenteMethod(function1, functionDerivative, 10);
+    cout << tangenteMethod(function1, functionDerivative, x0) << endl;
 
     return 0;
 }
